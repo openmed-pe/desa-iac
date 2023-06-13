2b41cc95bd91fd6a10a5afb51fe98cb22a7a1d56
Harold Gutierrez <haroldcohen.gutierrezbaca.contractor@gdevtools.com>
2023-06-13 17:29:10 -0500

    add acl ownership to s3

--- terraform/01.s3.tf
+++ terraform/01.s3.tf
@@ -13,3 +13,17 @@ resource "aws_s3_bucket_public_access_block" "openmed-desa-public-bucket" {
   block_public_policy = false
 
 }
+
+resource "aws_s3_bucket_ownership_controls" "openmed-desa-public-bucket-ownership" {
+  bucket = aws_s3_bucket.openmed-desa-public-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "openmed-desa-public-bucket-acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.openmed-desa-public-bucket-ownership]
+
+  bucket = aws_s3_bucket.openmed-desa-public-bucket.id
+  acl    = "private"
+}
