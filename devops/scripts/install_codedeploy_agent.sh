fedb5fed4231d2e1c075edd68e3d9536c6a00dae
Harold Gutierrez <haroldcohen.gutierrezbaca.contractor@gdevtools.com>
2023-06-16 12:14:13 -0500

    test

--- devops/scripts/install_codedeploy_agent.sh
+++ devops/scripts/install_codedeploy_agent.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt update
-sudo apt install ruby-full
+sudo apt install ruby-full -y
 sudo apt install wget
 cd /home/ubuntu
-wget https://aws-codedeploy-us-east-1.s3.region-identifier.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto > /tmp/logfile
 sudo service codedeploy-agent start
